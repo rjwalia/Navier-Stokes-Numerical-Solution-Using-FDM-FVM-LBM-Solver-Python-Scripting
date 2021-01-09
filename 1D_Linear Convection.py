82c42a8218735373cbb0c16d51ba4b13648d1b23
rjwalia <60730971+rjwalia@users.noreply.github.com>
2021-01-09 12:51:42 +0530

    Add files via upload

---
+++ 1D_Linear Convection.py
@@ -0,0 +1,39 @@
+import numpy                                                                #here we load numpy library that provides a bunch of useful matrix operations akin to MATLAB
+from matplotlib import pyplot                                               #2D plotting library that we will use to plot our results
+
+
+
+nx = 60                  #grip points
+dx = 4 / (nx -1)         #grid spacing 
+nt = 90                  #number of timesteps
+dt = 0.025               #timestep size
+c = 1                    #wavespeed 
+
+#innitial condition
+
+u = numpy.ones(nx)                #u = 1 across all grid points
+u[int(0.5/dx):int(1/dx+1)] = 2    #u = 2 from x = 0.5 to 1, Square Profile
+print(u)
+
+pyplot.plot(numpy.linspace(0, 4, nx), u, label='Initial Solution')
+
+#discritization
+
+un = numpy.ones(nx)                                       #initialize a temporary array, un is solution for next time step
+
+for n in range(nt):                                       #time marching
+    un = u.copy()                                         
+    for i in range(1, nx):                                #Space marching
+        u[i] = un[i] - c*dt/dx*(un[i] - un[i - 1])        #Backward Differnece Scheme
+
+
+
+print(u)
+pyplot.plot(numpy.linspace(0, 4, nx), u, label='Convected Solution')
+pyplot.title('1D Linear Convecction')
+pyplot.xlabel('Grid Space')
+pyplot.ylabel('Velocity')
+
+pyplot.legend()
+pyplot.show()
+
